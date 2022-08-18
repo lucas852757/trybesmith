99984b9614082c89a5eea431d645a827561e2fad
Lucas <bithome1@outlook.com>
2022-08-18 17:05:02 -0300

    adicionado rota /users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 /* https://app.betrybe.com/course/live-lectures/sd-cohort-19-b#dia-263-express-com-typescript */
 import express from 'express';
 import routesProducts from '../routes/routes.products';
+import routesUsers from '../routes/routes.users'; 
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', routesProducts);
+app.use('/users', routesUsers);
 
 export default app;
