9b4e7f18eeee2c8aafa8e6355cec97a20da5622c
Lucas <bithome1@outlook.com>
2022-08-17 22:46:51 -0300

    criado getAll

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,8 +1,9 @@
+/* https://github.com/tryber/praticando-typescript-express/blob/main/atividade-4/src/controllers/restaurantController.ts */
 import { Request, Response } from 'express';
 
 import ProductService from '../services/products.service';
 
-export default class ProductController {
+class ProductController {
   constructor(private productService = new ProductService()) {}
 
   public create = async (req: Request, res: Response) => {
@@ -10,4 +11,11 @@ export default class ProductController {
     const response = await this.productService.create(body);
     return res.status(201).json(response);
   };
+
+  public getAll = async (_req: Request, res: Response) => {
+    const response = await this.productService.getAll();
+    return res.status(200).json(response);
+  };
 }
+
+export default ProductController;
