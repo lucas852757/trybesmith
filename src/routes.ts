0159b2d82d3243b0c6a7679e4bb9435a0a12ee89
Lucas <bithome1@outlook.com>
2022-08-17 22:51:59 -0300

    getAll para a rota get

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import ProductController from './controllers/product.controller';
 const router = Router();
 const productController = new ProductController();
 
+router.get('/', productController.getAll);
 router.post('/', productController.create);
 
 export default router;
