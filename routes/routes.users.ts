f4a7bd1be73ff8e273a1983cae49c0c5c0e4c858
Lucas <bithome1@outlook.com>
2022-08-21 16:10:13 -0300

    atualizado

--- routes/routes.users.ts
+++ routes/routes.users.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import UserController from '../src/controllers/user.controller';
+import LoginController from '../src/controllers/login.controller';
 
 const router = Router();
 
 const userController = new UserController();
+const loginController = new LoginController();
 
+router.post('/login', loginController.login);
 router.post('/', userController.createUser);
 
 export default router;
