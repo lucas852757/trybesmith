7c85fc50f566cd48b985af5d66ad879c9eae76a2
Lucas <bithome1@outlook.com>
2022-08-22 15:51:57 -0300

    alteração

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,3 +1,4 @@
+import Joi from 'joi';
 import Product from '../../interfaces/product.interface';
 import Units from '../../interfaces/units.interface';
 import connection from '../models/connection';
@@ -11,6 +12,13 @@ export default class ProductService {
   }
 
   public async create(object: Units): Promise<Product> {
+    const { name, amount } = object;
+    const schema = Joi.object({ 
+      name: Joi.string().min(3).not().empty()
+        .required(), 
+      amount: Joi.string().min(3).not().empty()
+        .required() });
+    await schema.validateAsync({ name, amount });
     const result = await this.model.create(object);
     return result;
   }
