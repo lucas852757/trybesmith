319faebac79236834f6721fe8d9fa72747a967dd
Lucas <bithome1@outlook.com>
2022-08-22 17:34:04 -0300

    adicionado try cath

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,17 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import UserService from '../services/users.service';
 
 export default class UserController {
   constructor(private userService = new UserService()) {}
 
-  public createUser = async (req: Request, res: Response) => {
-    const { body } = req;
-    const response = await this.userService.createUser(body);
-    return res.status(201).json(response);
+  public createUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { body } = req;
+      const response = await this.userService.createUser(body);
+      return res.status(201).json(response);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   };
 }
