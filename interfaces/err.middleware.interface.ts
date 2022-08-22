9c88335a75ad653ae18c0b3c973a167ee81b0698
Lucas <bithome1@outlook.com>
2022-08-22 15:40:57 -0300

    adicionado novas propriedades

--- interfaces/err.middleware.interface.ts
+++ interfaces/err.middleware.interface.ts
@@ -3,6 +3,9 @@ import express from 'express';
 interface ErrorMiddleware extends express.ErrorRequestHandler {
   name: string,
   message: string,
+  isJoi: object[],
+  status: number,
+  details: [{ message:string, path:string[], type:string, context: object }],
 }
 
 export default ErrorMiddleware;
