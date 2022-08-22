a3705464692178295980695f626f703b2174dac9
Lucas <bithome1@outlook.com>
2022-08-21 22:31:29 -0300

    adicionado try catch

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,15 +1,20 @@
 /* https://github.com/tryber/praticando-typescript-express/blob/main/atividade-4/src/controllers/restaurantController.ts */
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import ProductService from '../services/products.service';
 
 class ProductController {
   constructor(private productService = new ProductService()) {}
 
-  public create = async (req: Request, res: Response) => {
-    const { body } = req;
-    const response = await this.productService.create(body);
-    return res.status(201).json(response);
+  public create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { body } = req;
+      const response = await this.productService.create(body);
+      return res.status(201).json(response);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   };
 
   public getAll = async (_req: Request, res: Response) => {
