86da73fe9435b396976965e813da87de50134b6a
Lucas <bithome1@outlook.com>
2022-08-23 16:43:09 -0300

    adicionado método create

--- routes/routes.orders.ts
+++ routes/routes.orders.ts
@@ -2,9 +2,9 @@ import { Router } from 'express';
 import OrderController from '../src/controllers/order.controller';
 
 const router = Router();
+const orderController = new OrderController();
 
-const userController = new OrderController();
-
-router.get('/', userController.getAll);
+router.get('/', orderController.getAll);
+router.post('/', orderController.create);
 
 export default router;
